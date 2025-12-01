33b06fedc9a3ee2c7bc84afcaf9b1c00e8d0bade
dhwlpandey <dhwlpandey@gmail.com>
2025-12-01 21:31:04 +0530

    feat: implement master play audio controls and progress bar listener, and fix progress bar ID in CSS

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 console.log("Welcome to Spotify")
+
+//Intialise the Variables
+let songIndex = 0;
 let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+
+let songs = [
+    {songName: "song-1", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "song-1", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "song-1", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "song-1", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "song-1", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "song-1", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "song-1", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "song-1", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "song-1", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "song-1", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+]
 // audioElement.play();
+
+// Handle play/pause click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+    }
+    else{
+        audioElement.pause();
+    }
+})
+// Listen to Events
+myProgressBar.addEventListener('timeupdate', ()=>{
+    console.log('timeupdate');
+    //update seekbar
+})
